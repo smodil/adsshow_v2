b01ab00106ece8ad04685af67351b6ca2e2f41ac
smodil <149347603+smodil@users.noreply.github.com>
2023-12-09 14:30:04 +0530

    Add files via upload

---
+++ accounts/key/A02N293JIFJ93290R9FJ92J9WJ0DF9F9JW0RJ2890R/ads.js
@@ -0,0 +1,43 @@
+var time = document.getElementById("time");
+setTimeout(function(){
+  
+  time.innerHTML = "6";
+  
+}, 1000);
+
+setTimeout(function() {
+
+  time.innerHTML = "5";
+
+}, 2000);
+setTimeout(function() {
+
+  time.innerHTML = "4";
+
+}, 3000);
+setTimeout(function() {
+
+  time.innerHTML = "3";
+
+}, 4000);
+setTimeout(function() {
+
+  time.innerHTML = "2";
+
+}, 5000);
+setTimeout(function() {
+
+  time.innerHTML = "1";
+
+}, 6000);
+setTimeout(function() {
+
+  time.innerHTML = "0";
+
+}, 7000);
+setTimeout(function() {
+
+ document.getElementById("time_line").innerHTML = "Scroll down and hit continue";
+      
+document.getElementById("nav").innerHTML = '<a href="A1001010111011111010101111010101.html" style="background: #6a5acd;">Continue</a>';
+}, 8000);
